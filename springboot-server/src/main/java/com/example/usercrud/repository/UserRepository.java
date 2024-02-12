db53d9b9a9aeca2b5146c3245f0f2e553ca57cc3
technologicaljerry <cserajatverma@gmail.com>
2024-02-12 23:22:53 +0530

    Delete UserRepository.java

--- springboot-server/src/main/java/com/example/usercrud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.usercrud.repository;
-
-import com.example.usercrud.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
