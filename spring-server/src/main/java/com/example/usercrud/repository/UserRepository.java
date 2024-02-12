f483c21c851ed1534e8cd38bc63c38d9a121579d
technologicaljerry <cserajatverma@gmail.com>
2024-02-12 23:21:49 +0530

    Create UserRepository.java

---
+++ spring-server/src/main/java/com/example/usercrud/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.usercrud.repository;
+
+import com.example.usercrud.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
