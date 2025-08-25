9217d97b956addfd27b1f685ce07998638c3f660
TechnologicalJerry <cserajatverma@gmail.com>
2025-08-25 20:01:32 +0530

    Update app.routes.ts

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'signup',
+        component: Signup
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
