f2735a06e359674f9061a35a5c9c0f0e4a89800b
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-16 23:06:55 +0530

    Create TailwindAngularSpringMysqlApplication.java

---
+++ spring-server/src/main/java/com/technojerry/tailwind_angular_spring_mysql/TailwindAngularSpringMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.technojerry.tailwind_angular_spring_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailwindAngularSpringMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailwindAngularSpringMysqlApplication.class, args);
+	}
+
+}
