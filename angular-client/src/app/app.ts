eb42ced8bdb4d4c2a805f0328c130244ff4e4b30
TechnologicalJerry <cserajatverma@gmail.com>
2025-10-18 22:25:39 +0530

    Remove Header and Footer imports from AppComponent
    The Header and Footer components were removed from the imports array in AppComponent. This change may be part of a refactor to simplify the root component or to relocate these components elsewhere.

--- angular-client/src/app/app.ts
+++ angular-client/src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
