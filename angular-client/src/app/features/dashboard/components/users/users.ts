c6894d2b45e6eb05e0a288d01a4d555c17c569bf
TechnologicalJerry <cserajatverma@gmail.com>
2025-12-04 23:21:51 +0530

    Update users component to use CSS stylesheet
    Changed the styleUrl in the Users component from 'users.scss' to 'users.css' to reference the correct stylesheet format.

--- angular-client/src/app/features/dashboard/components/users/users.ts
+++ angular-client/src/app/features/dashboard/components/users/users.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-users',
   imports: [],
   templateUrl: './users.html',
-  styleUrl: './users.scss'
+  styleUrl: './users.css',
 })
 export class Users {
 
